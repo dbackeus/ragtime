6071b011e9432f38cd5bcac0240b8caeaf0951b0
David Backeus <duztdruid@gmail.com>
2013-04-12 17:27:17 +0200

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Raganow::Application.config.secret_token = '8a3297ede0d02194da0742bae216c09403e7b9b97adbc5cb6d17b2f164b2a21ba083a7433bb5f98360495d36d381500b083880287967f74dcd954b297ce27f3f'
