ac02e1870daebd3a08378ebece16df49986cd505
David Backeus <duztdruid@gmail.com>
2013-04-25 16:53:44 +0200

    Chakra icons for list.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,7 +10,7 @@ module ApplicationHelper
 
   def chakra_image(chakra)
     chakra = chakra.split("_").last
-    image_tag "chakra_#{chakra}.jpg", :alt => ""
+    image_tag "chakra_#{chakra}.jpg", :alt => "", class: "chakra-image"
   end
   
   def pretty_clock
