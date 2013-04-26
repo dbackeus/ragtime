a2fffbe97612f80bed70a10804cf66aea96634e0
David Backeus <duztdruid@gmail.com>
2013-04-26 13:47:28 +0200

    Missing config/sitemap.rb.

---
+++ config/sitemap.rb
@@ -0,0 +1,25 @@
+# Set the host name for URL creation
+SitemapGenerator::Sitemap.default_host = "http://raga.herokuapp.com"
+
+SitemapGenerator::Sitemap.create do
+  Raga.all.each do |raga|
+    add raga_path(raga), lastmod: raga.updated_at
+  end
+  # Put links creation logic here.
+  #
+  # The root path '/' and sitemap index file are added automatically for you.
+  # Links are added to the Sitemap in the order they are specified.
+  #
+  # Usage: add(path, options={})
+  #        (default options are used if you don't specify)
+  #
+  # Defaults: :priority => 0.5, :changefreq => 'weekly',
+  #           :lastmod => Time.now, :host => default_host
+  #
+  # Examples:
+  #
+  # Add '/articles'
+  #
+  #   add articles_path, :priority => 0.7, :changefreq => 'daily'
+  #
+end
