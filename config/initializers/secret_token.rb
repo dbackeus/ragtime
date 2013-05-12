ebc0572703b5131abdc47b5e17d42e9e6181d9b6
David Backeus <duztdruid@gmail.com>
2013-05-12 12:30:07 +0200

    Secret key base.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Raganow::Application.config.secret_token = '8a3297ede0d02194da0742bae216c09403e7b9b97adbc5cb6d17b2f164b2a21ba083a7433bb5f98360495d36d381500b083880287967f74dcd954b297ce27f3f'
+Raganow::Application.config.secret_key_base = 'f3f72ec792b459dcd47f769782088380b005183d63d59406389f5bb3347a380ab12a2b461f2b71d6bc5cbda79b9b7e30490c612eab2470ad49120d0ede7923a8'
