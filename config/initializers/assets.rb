aa85fde4d01c9f46654adcb5d72c6e4a125331b5
David Backeus <duztdruid@gmail.com>
2015-02-28 18:51:39 +0100

    Fix assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,8 +4,8 @@
 Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
-config.assets.paths << Rails.root.join("app", "assets", "flash")
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "flash")
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-config.assets.precompile += %w[ragas.js]
+Rails.application.config.assets.precompile += %w[ragas.js]
